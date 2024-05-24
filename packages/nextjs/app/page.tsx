5aa7f0ad432fa0df788e7c78c5c320a50bd068ba
nnn-gif <nicky.gurbani@gmail.com>
2024-05-24 12:19:40 +0530

    return post IPFS address

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import Link from "next/link";
 import type { NextPage } from "next";
 import { useAccount } from "wagmi";
+import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { Address } from "~~/components/scaffold-eth";
 import { createNewPost } from "~~/services/web3/signMessage";
 import React, { useState } from 'react';
@@ -9,72 +11,28 @@ import React, { useState } from 'react';
 
 const Home: NextPage = () => {
   const { address: connectedAddress } = useAccount();
-  const [inputValue, setInputValue] = useState("");  
-  const [outputValue, setOutputValue] = useState("");  
+  const [inputValue, setInputValue] = useState("");
+  const [outputValue, setOutputValue] = useState("");
 
 
 
   const createPost = async () => {
     // Replace this with your actual createPost logic
     const postcid = await createNewPost(inputValue)
-    setOutputValue(postcid.data.Hash); // Store the result in the outputValue state
+    setOutputValue("Post IPFS Address: " + postcid.data.Hash); // Store the result in the outputValue state
 
     console.log("Input value:", inputValue);
-    
+
   };
 
   return (
     <>
       <div className="flex items-center flex-col flex-grow pt-10">
-        <div className="px-5">
-          <h1 className="text-center">
-            <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
-          </h1>
-          <div className="flex justify-center items-center space-x-2">
-            <p className="my-2 font-medium">Connected Address:</p>
-            <Address address={connectedAddress} />
-          </div>
-          <p className="text-center text-lg">
-            Get started by editing{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/nextjs/app/page.tsx
-            </code>
-          </p>
-          <p className="text-center text-lg">
-            Edit your smart contract{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              YourContract.sol
-            </code>{" "}
-            in{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/hardhat/contracts
-            </code>
-          </p>
-        </div>
+
 
         <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
           <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <BugAntIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Tinker with your smart contract using the{" "}
-                <Link href="/debug" passHref className="link">
-                  Debug Contracts
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Explore your local transactions with the{" "}
-                <Link href="/blockexplorer" passHref className="link">
-                  Block Explorer
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
+
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
               <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
               <input
@@ -82,21 +40,22 @@ const Home: NextPage = () => {
                 value={inputValue}
                 onChange={(e) => setInputValue(e.target.value)}
                 className="mt-4 p-2 border rounded"
-                placeholder="Enter your message"
+                placeholder="Post Body"
               />
-              <p>
-        Sign Message{" "}
-        <button onClick={createPost} className="link">
-          Block Explorer
-        </button>{" "}
-        <div className="mt-4">
-        {outputValue && <p>{outputValue}</p>}  
-      </div>
-      </p>
+              <div>
+
+                <button onClick={createPost} className="link">
+                  Create Post{" "}
+                </button>{" "}
+                <div className="mt-4">
+                  {outputValue && <p>{outputValue}</p>}
+                </div>
+              </div>
             </div>
           </div>
         </div>
-        <Address address={connectedAddress} />
+
+
       </div>
     </>
   );
